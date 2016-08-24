16f58bffe342510bc9dcac9d884c31a9d02dccc4
Matthew H <mhung@example.com>
2016-08-24 21:44:47 +0800

    first migration of creating Product scaffold and changed price precision and scale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '901bc1f732561af5194a00065cffea4279fdd9c8ff59df53e3083bdec22d940f7c2079fff1e4adfba4122ba8cdd09de67cb7c9bebeaefae5a2355b053e43a96a'
