23be1e3ba51005ee347d24f63c21ae83ea405f75
dira m <diramarid@gmail.com>
2016-09-26 10:59:35 +0800

    fixed several typo to eliminate one rake test:controllers error, there is one error left

--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 20160901122612) do
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
 
   create_table "products", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",       limit: 255
     t.text     "description"
-    t.string   "image_url"
-    t.decimal  "price",       precision: 8, scale: 2
+    t.string   "image_url",   limit: 255
+    t.decimal  "price",                   precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
